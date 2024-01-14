31ec4abac018f4b2c24d1efbfd860d139b468ed5
K VINOD <74367067+kamivinnu@users.noreply.github.com>
2024-01-14 11:33:13 +0530

    Update 1_if_else.cpp

--- 3_Conditional_and_Loop_Functionality/1_if_else.cpp
+++ 3_Conditional_and_Loop_Functionality/1_if_else.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main() {
   int n;
+  // cin is special variable that is used to take input from user
+  // cin - tab, enter, space, etc.
   cin >> n;
   // cout << "Value of n : " << n << endl;
   if (n > 0) {
