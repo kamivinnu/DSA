ce1df57e720a4c68d124c732de7b042a77491a76
K VINOD <74367067+kamivinnu@users.noreply.github.com>
2024-01-14 10:24:59 +0530

    Create 3_neg_values.cpp

---
+++ 2_write_your_first_program_in_C++/3_neg_values.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // unsigned means positive numbers only 
+  unsigned int a = -10;
+  cout << a << endl;
+
+  int b = -112;
+  cout << b << endl;
+}
