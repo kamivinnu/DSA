67e64eabd845dc78252b44d5f33c7d69be168ac4
K VINOD <74367067+kamivinnu@users.noreply.github.com>
2024-01-14 08:19:11 +0530

    Create 1_first_program.cpp

---
+++ 2_write_your_first_program_in_C++/1_first_program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl; 
+
+  int a = 5;
+  int size = sizeof(a);
+  cout << "size of a is: " << size << endl;
+
+
+}
