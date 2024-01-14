bc6dcd537ae2d70725c0f996ea3d3929b5f30dd9
K VINOD <74367067+kamivinnu@users.noreply.github.com>
2024-01-14 11:17:19 +0530

    Create 1_if_else.cpp

---
+++ 3_Conditional_and_Loop_Functionality/1_if_else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  // cout << "Value of n : " << n << endl;
+  if (n > 0) {
+    cout << "N is Positive" << endl;
+  } 
+  else {
+    cout << "N is Negative" << endl;
+  }
+}
